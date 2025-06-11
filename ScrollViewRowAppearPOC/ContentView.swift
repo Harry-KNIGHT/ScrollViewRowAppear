a4e1d0eda992bc987de780826adba8e2d6b212d8
Harry KNIGHT <eliott.knight@icloud.com>
2025-06-11 07:02:31 +0200

    Initial Commit

---
+++ ScrollViewRowAppearPOC/ContentView.swift
@@ -0,0 +1,87 @@
+//
+//  ContentView.swift
+//  ScrollViewRowAppearPOC
+//
+//  Created by Elliot Knight on 11/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var visibles: [Int] = []
+    let sampleItems = (0..<100).map { Item(number: $0) }
+    
+    var body: some View {
+        
+        ScrollView {
+            LazyVStack {
+                ForEach(sampleItems) { num in
+                    Text("number: \(num.name)")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .padding(.horizontal)
+                        .anchorPreference(
+                            key: VisibleItemsPreference.self,
+                            value: .bounds,
+                            transform: { anchor in
+                                [.init(item: num, bounds: anchor)]
+                        })
+                        
+                    
+                }
+            }
+        }
+        .overlayPreferenceValue(VisibleItemsPreference.self, { value in
+            GeometryReader { proxy in
+                        let myFrame = proxy.frame(in: .local)
+                        let arr = value.sorted(by: { $0.item.number < $1.item.number }).map { item in
+                            let inBounds = myFrame.intersects(proxy[item.bounds])
+                            return (inBounds: inBounds, item: item.item)
+                        }
+                        let texts: [Text] = arr.map { (inBounds: Bool, item: Item) in
+                            Text("\(item.number)")
+                                .foregroundStyle(inBounds ? .primary : .secondary)
+                        }
+                        texts.joined(separator: Text(","))
+                            .foregroundStyle(.white)
+                            .background(.black)
+                            .frame(maxHeight: .infinity)
+                    }
+                })
+        
+        
+    }
+}
+
+extension [Text] {
+    func joined(separator: Text) -> Text {
+        guard let f = first else { return Text("") }
+        return dropFirst().reduce(f, { $0 + separator + $1 })
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct Item: Identifiable, Hashable {
+    var id = UUID()
+    var number: Int
+
+    var name: String {
+        "Item \(number)"
+    }
+}
+
+struct Payload {
+    var item: Item
+    var bounds: Anchor<CGRect>
+}
+
+struct VisibleItemsPreference: PreferenceKey {
+    static var defaultValue: [Payload] = []
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value.append(contentsOf: nextValue())
+    }
+}
---
+++ ScrollViewRowAppearPOC/ScrollViewRowAppearPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewRowAppearPOCApp.swift
+//  ScrollViewRowAppearPOC
+//
+//  Created by Elliot Knight on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewRowAppearPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
